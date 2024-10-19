ff757ab2a01162c97393d76231731e6fed6d30b6
Ashoksai-tech <aashoksai306@gmail.com>
2024-10-19 23:08:55 +0530

    files

--- src/main.py
+++ src/main.py
@@ -8,6 +8,7 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 from firebase_admin import storage
+from datetime import datetime
 
 
 
@@ -16,6 +17,7 @@ firebase_admin.initialize_app(cred,{
     'databaseURL':"https://real-time-face-attendanc-142ca-default-rtdb.firebaseio.com/",
     'storageBucket':"real-time-face-attendanc-142ca.appspot.com"
 })
+bucket = storage.bucket()
 
 # set the width and height of frame
 cap = cv2.VideoCapture(0)
@@ -48,6 +50,7 @@ print("encode file loaded")
 modetype = 0
 counter = 0
 id = -1
+imgstudent = []
 
 
 
@@ -70,40 +73,115 @@ while True:
 
     # add webcam to background
     imgbackground[162:162+480,55:55+640] = img
-    imgbackground[44:44+633,808:808+414] = imgmodlist[0]
+    imgbackground[44:44+633,808:808+414] = imgmodlist[modetype]
 
 
 #check for generated encoding whether there are any matches
-    for encodeface,facelocation in zip(encodecurframe,facecurframe):
-        matches = face_recognition.compare_faces(encodelistknown,encodeface)
-        facedis = face_recognition.face_distance(encodelistknown,encodeface)
-        # print("matches",matches)
-        # print("facedis",facedis)
+    if facecurframe:
+        for encodeface,facelocation in zip(encodecurframe,facecurframe):
+            matches = face_recognition.compare_faces(encodelistknown,encodeface)
+            facedis = face_recognition.face_distance(encodelistknown,encodeface)
+            # print("matches",matches)
+            # print("facedis",facedis)
 
 
-        matchindex = np.argmin(facedis)
-        #print("Match Index",matchindex)
+            matchindex = np.argmin(facedis)
+            #print("Match Index",matchindex)
 
 
-        if matches[matchindex]:
-            print("known face detected")
-            print(stids[matchindex])
-            y1,x2,y2,x1 = facelocation
-            y1,x2,y2,x1 = y1*4, x2*4, y2*4, x1*4
-            bbox = 55+x1,162+y1,x2-x1,y2-y1
-            imgbackground = cvzone.cornerRect(imgbackground,bbox,rt=0)
-            id = stids[matchindex]
-            print(id)
+            if matches[matchindex]:
+                print("known face detected")
+                print(stids[matchindex])
+                y1,x2,y2,x1 = facelocation
+                y1,x2,y2,x1 = y1*4, x2*4, y2*4, x1*4
+                bbox = 55+x1,162+y1,x2-x1,y2-y1
+                imgbackground = cvzone.cornerRect(imgbackground,bbox,rt=0)
+                id = stids[matchindex]
+                print(id)
 
-            if counter==0:
-                counter=1
-                modetype=1
+                if counter==0:
+                    counter=1
+                    modetype=1
 
 
-    if counter!=0:
-        if counter ==1:
-            studentinfo = db.reference(f'Students/{id}').get()
-            print(studentinfo)
+        if counter!=0:
+            if counter ==1:
+                # get the data
+                studentinfo = db.reference(f'Students/{id}').get()
+                print(studentinfo)
+                
+                #get the image from storage
+
+                blob = bucket.get_blob(f'images\{id}.png')
+                array = np.frombuffer(blob.download_as_string(),np.uint8)
+                imgstudent = cv2.imdecode(array,cv2.COLOR_BGRA2BGR)
+
+                #update data of attendance
+                datetimeobject = datetime.strptime(studentinfo['last attendance time'],
+                                            "%Y-%m-%d %H:%M:%S")
+                seconds = (datetime.now() - datetimeobject).total_seconds()
+                print(seconds)
+                if seconds > 86400:
+                    ref =db.reference(f'Students/{id}')
+                    studentinfo['total attendance'] +=1
+                    ref.child('total attendance').set(studentinfo['total attendance'])
+                    ref.child('last attendance time').set(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+
+                else:
+                    modetype = 3
+                    counter = 2
+                    imgbackground[44:44 + 633, 808:808 + 414] = imgmodlist[modetype]
+
+
+
+            if modetype!=3:
+                if 10<counter<20:
+                    modetype=2
+
+                imgbackground[44:44 + 633, 808:808 + 414] = imgmodlist[modetype]
+
+                if counter<=10:
+
+                    cv2.putText(imgbackground,str(studentinfo['total attendance']),(861,125),
+                                cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),1)
+                    
+                    cv2.putText(imgbackground,str(studentinfo['Major']),(1006,550),
+                                cv2.FONT_HERSHEY_COMPLEX,0.5,(255,255,255),1)
+                    cv2.putText(imgbackground,str(id),(1006,493),
+                                cv2.FONT_HERSHEY_COMPLEX,0.5,(255,255,255),1)
+                    cv2.putText(imgbackground,str(studentinfo['standing']),(910,625),
+                                cv2.FONT_HERSHEY_COMPLEX,0.6,(100,100,100),1)
+                    cv2.putText(imgbackground,str(studentinfo['Year']),(1025,625),
+                                cv2.FONT_HERSHEY_COMPLEX,0.6,(100,100,100),1)
+                    cv2.putText(imgbackground,str(studentinfo['starting year']),(1125,625),
+                                cv2.FONT_HERSHEY_COMPLEX,0.6,(100,100,100),1)
+                    
+                    (w, h), _ = cv2.getTextSize(studentinfo['name'],cv2.FONT_HERSHEY_COMPLEX,1,1)
+                    offset = (414-w)//2
+                    
+
+                    cv2.putText(imgbackground,str(studentinfo['name']),(808+offset,445),
+                                cv2.FONT_HERSHEY_COMPLEX,1,(50,50,50),1)
+                    
+
+                    imgbackground[175:175+216,909:909+216] = imgstudent
+
+
+            counter +=1
+
+            if counter>=20:
+                counter = 0
+                modetype = 0
+                studentinfo = []
+                imgstudent = []
+                imgbackground[175:175+216,909:909+216] = imgstudent
+
+    
+    else:
+        modetype = 0
+        counter = 0
+        
+
 
 
 
